c7c90bcf8b542b69c0d2324e0630d66a157d7a91
Willis Morse <willis.morse@thermofisher.com>
2020-09-14 10:11:25 -0400

    Generates a ramp wave

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
+import * as fs from "fs";
+
 console.log("Started main");
+
+import { WaveFile } from 'wavefile';
+let wav = new WaveFile();
+
+let sampleRate = 44100.0;
+
+const halfSecond = sampleRate / 2;
+
+
+let dataBuffer: Array<number> = Array(halfSecond * 2).fill(0);
+
+for (let i = 0; i < halfSecond; i++) {
+    dataBuffer[i] = i / halfSecond;
+}
+for (let i = 0; i < halfSecond; i++) {
+    dataBuffer[i + halfSecond] = 1.0 - (i / halfSecond);
+}
+
+
+
+
+wav.fromScratch(1, sampleRate, '32f', dataBuffer);
+
+const generatedDirPath = './generated';
+if (!fs.existsSync(generatedDirPath)){
+    fs.mkdirSync(generatedDirPath);
+}
+
+let dataFilePath = `${generatedDirPath}/triangle-one-second.wav`;
+fs.writeFileSync(dataFilePath, wav.toBuffer());
